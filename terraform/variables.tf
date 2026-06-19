423146027854e74bd8c7ab8cdfbb7250b96e5599
Michael Cowell <spike@spikeandbetty.com>
2026-06-19 16:21:44 -0400

    Add Terraform resource group infrastructure

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "rg-access-request-platform"
+}
+
+variable "location" {
+  default = "eastus"
+}
+
+variable "app_name" {
+  default = "access-request-platform"
+}

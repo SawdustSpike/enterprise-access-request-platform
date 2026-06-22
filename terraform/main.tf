f4c621b42101103e0551f06c71035ad4744edf9a
Michael Cowell <spike@spikeandbetty.com>
2026-06-22 15:58:24 -0400

    Deployed application infrastructure with Azure Container Apps and PostgreSQL

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,32 @@ resource "azurerm_container_app" "main" {
       image  = "${azurerm_container_registry.main.login_server}/access-request-platform:latest"
       cpu    = 0.5
       memory = "1Gi"
+
+      env {
+        name  = "SPRING_DATASOURCE_URL"
+        value = "jdbc:postgresql://${azurerm_postgresql_flexible_server.main.fqdn}:5432/accessplatform?sslmode=require"
+      }
+
+      env {
+        name  = "SPRING_DATASOURCE_USERNAME"
+        value = var.postgres_admin_username
+      }
+
+      env {
+        name        = "SPRING_DATASOURCE_PASSWORD"
+        secret_name = "postgres-password"
+      }
     }
+  }
+
+  secret {
+    name  = "postgres-password"
+    value = var.postgres_admin_password
+  }
+
+  secret {
+    name  = "acr-password"
+    value = azurerm_container_registry.main.admin_password
   }
 
   ingress {
@@ -44,9 +69,32 @@ resource "azurerm_container_app" "main" {
     username             = azurerm_container_registry.main.admin_username
     password_secret_name = "acr-password"
   }
+}
+resource "azurerm_postgresql_flexible_server" "main" {
+  name                   = "psql-access-platform-central"
+  resource_group_name    = azurerm_resource_group.main.name
+  location               = var.postgres_location
+  version                = "16"
+  administrator_login    = var.postgres_admin_username
+  administrator_password = var.postgres_admin_password
+  sku_name               = "B_Standard_B1ms"
+  storage_mb             = 32768
+  zone                   = "3"
 
-  secret {
-    name  = "acr-password"
-    value = azurerm_container_registry.main.admin_password
-  }
+
+  public_network_access_enabled = true
+}
+
+resource "azurerm_postgresql_flexible_server_database" "main" {
+  name      = "accessplatform"
+  server_id = azurerm_postgresql_flexible_server.main.id
+  charset   = "UTF8"
+  collation = "en_US.utf8"
+}
+
+resource "azurerm_postgresql_flexible_server_firewall_rule" "azure_services" {
+  name             = "AllowAzureServices"
+  server_id        = azurerm_postgresql_flexible_server.main.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,9 +3,21 @@ variable "resource_group_name" {
 }
 
 variable "location" {
-  default = "eastus"
+  default = "eastus2"
+}
+
+variable "postgres_location" {
+  default = "centralus"
 }
 
 variable "app_name" {
   default = "access-request-platform"
+}
+
+variable "postgres_admin_username" {
+  default = "pgadminuser"
+}
+
+variable "postgres_admin_password" {
+  sensitive = true
 }

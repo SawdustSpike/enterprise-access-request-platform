0770dea961fe47784b74175e2c297e18faa48fe3
Michael Cowell <spike@spikeandbetty.com>
2026-06-22 13:08:07 -0400

    Deploy application to azure container apps  with Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,51 @@ resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 }
+resource "azurerm_container_registry" "main" {
+  name                = "acraccessrequestplatform"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+resource "azurerm_container_app_environment" "main" {
+  name                = "cae-access-request-platform"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+resource "azurerm_container_app" "main" {
+  name                         = "access-request-platform"
+  container_app_environment_id = azurerm_container_app_environment.main.id
+  resource_group_name          = azurerm_resource_group.main.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "access-request-platform"
+      image  = "${azurerm_container_registry.main.login_server}/access-request-platform:latest"
+      cpu    = 0.5
+      memory = "1Gi"
+    }
+  }
+
+  ingress {
+    external_enabled = true
+    target_port      = 8080
+
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+  }
+
+  registry {
+    server               = azurerm_container_registry.main.login_server
+    username             = azurerm_container_registry.main.admin_username
+    password_secret_name = "acr-password"
+  }
+
+  secret {
+    name  = "acr-password"
+    value = azurerm_container_registry.main.admin_password
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,9 @@
 output "resource_group_name" {
   value = azurerm_resource_group.main.name
 }
+output "container_registry_login_server" {
+  value = azurerm_container_registry.main.login_server
+}
+output "container_app_url" {
+  value = azurerm_container_app.main.latest_revision_fqdn
+}
